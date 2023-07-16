4277b439236a3682561e827615da3c484c71bbeb
SirGFM <gfmandaji@hotmail.com>
2023-07-15 21:12:18 -0300

    next.config: Add dev proxy setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const prodNextConfig = {
 	output: 'export',
 	distDir: 'build',
 };
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const devNextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/:path*',
+				destination: process.env.API_URL + '/:path*',
+			},
+		];
+	},
+};
+
+if (process.env.NODE_ENV == 'development') {
+	module.exports = devNextConfig;
+} else {
+	module.exports = prodNextConfig;
+}
